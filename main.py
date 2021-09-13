8ab779ebb488f42ac3dba18d427ecd306e618323
Steve Cline <steve.cline@weldre4.org>
2021-09-13 12:50:29 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+
+led = 12 # GPIO pin number is 5 and name is GPIO3
+
+GPIO.setup(led, GPIO.OUT, initial = 0) # Setup LED and set it initially to OFF
+
+while(True):
+        GPIO.output(led, GPIO.HIGH) # Set LED to ON
+        time.sleep(2) # Wait for 2 seconds
+
+        GPIO.output(led, GPIO.LOW) # Set LED to OFF
+        time.sleep(2)
