f30f9956fe7e578f12ddeb61fc2717b55c0f4b4b
Steve Cline <steve.cline@weldre4.org>
2024-01-18 15:49:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
-led = 12 # GPIO pin number is 5 and name is GPIO3
+led = 12 # GPIO pin number is 12 and name is GPIO18
 
 GPIO.setup(led, GPIO.OUT, initial = 0) # Setup LED and set it initially to OFF
 
